c81190c87ac332a439acd4c05c330d70bf925cf1
Vladick <cool.valad12@gmail.com>
2022-10-21 02:34:43 +0300

    init commit

---
+++ src/main/java/com/epam/bragin/BraginApplication.java
@@ -0,0 +1,13 @@
+package com.epam.bragin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BraginApplication.class, args);
+	}
+
+}
